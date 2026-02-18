c26551e9e15ce65ea2d479e44117827da5aaf4d6
Zeddy Vazquez <zeddy23yoo@gmail.com>
2026-02-17 22:02:05 -0600

    Subiendo prog.sh

---
+++ prog.sh
@@ -0,0 +1,45 @@
+#suma de dos numeros en bash
+
+tput setaf 2
+
+echo "ingrese el primer numero: "
+read num1
+
+echo "Ingrese el segundo numero: "
+read num2
+
+#Realizar la suma
+
+suma=$((num1 + num2))
+
+echo "La suma de $num1 y $num2 es: $suma"
+
+saludo="Hola Mundo"
+echo "#######################################################################"
+echo "Primer  $saludo"
+tput setaf 6
+
+#Multiplicacion de dos numeros
+
+echo "Ingrese el primer numero: "
+read num1
+echo "Ingrese el segundo numero: "
+read num2
+#Realizando la multiplicacion
+
+multi=$((num1 * num2))
+echo "La multiplicacion de $num1 y $num2 es: $multi"
+
+#Agregando la resta a este programa
+
+echo "Esta es la resta de dos numeros"
+echo "Ingresa el primer numero: "
+read num1
+
+echo "Ingresa el segundo numero: "
+read num2
+
+#Realizar la resta
+resta=$((num1 - num2))
+echo "La resta de $num1 y $num2 es: $resta"
+
